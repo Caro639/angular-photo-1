3ac8a04e10707d52096b6988d47801fb556fc40a
Caro639 <120673222+Caro639@users.noreply.github.com>
2025-11-01 17:26:47 +0100

    add array+@for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,37 @@ import { FaceSnap } from "./models/face-snap";
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
+  faceSnaps!: FaceSnap[];
+
   mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
 
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      "Mon premier Ami",
-      "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
-      "Mon premier Ami depuis Angular",
-      new Date(),
-      0
+    this.faceSnaps = [
+      new FaceSnap(
+        "Mon premier Ami",
+        "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
+        "Mon premier Ami depuis Angular",
+        new Date(),
+        0
+      ),
+      new FaceSnap(
+        "Three Rock Mountain",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg",
+        "Un endroit magnifique pour les randonnées.",
+        new Date(),
+        6
+      ),
+      new FaceSnap(
+        "Un bon repas",
+        "https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg",
+        "Mmmh que c'est bon !",
+        new Date(),
+        156
+      ),
+    ];
+    this.faceSnaps[1].setLocation(
+      "à la montagne"
     );
   }
 }
